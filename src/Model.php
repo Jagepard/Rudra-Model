a252dfb6b40dcc66776bdc431bc97d165c498881
Jagepard <dankorot@gmail.com>
2017-04-07 10:37:17 +0300

    Updated

--- src/Model.php
+++ src/Model.php
@@ -4,10 +4,6 @@ declare(strict_types = 1);
 
 namespace Rudra;
 
-
-use App\Config;
-
-
 /**
  * Class Model
  *
@@ -31,8 +27,6 @@ class Model
     public function __construct(IContainer $container)
     {
         $this->container = $container;
-//        Container::$app->get('debugbar')['time']->stopMeasure('Controller');
-//        Container::$app->get('debugbar')['time']->startMeasure('Model', __CLASS__);
     }
 
     /**
@@ -41,23 +35,5 @@ class Model
     public function container()
     {
         return $this->container;
-    }
-
-    /**
-     * @param $key
-     *
-     * @return string
-     */
-    public function fileUpload($key)
-    {
-        if ($this->container()->isUploaded($key)) {
-            $uploadedFile = '/uploaded/' . substr(md5(microtime()), 0, 5) . $this->container()->getUpload($key, 'name');
-            $uploadPath   = Config::PUBLIC_PATH . $uploadedFile;
-            move_uploaded_file($this->container()->getUpload($key, 'tmp_name'), $uploadPath);
-
-            return APP_URL . $uploadedFile;
-        }
-
-        return $this->container()->getPost($key);
     }
 }
