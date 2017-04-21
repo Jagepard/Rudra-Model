a875ba407e35331a5910d644ec86664fc6c1dbae
Jagepard <dankorot@gmail.com>
2017-04-21 13:02:47 +0300

    Updated

--- src/Model.php
+++ src/Model.php
@@ -12,28 +12,7 @@ namespace Rudra;
 class Model
 {
 
-    use ContainerTrait, DataTrait;
-
-    /**
-     * @var
-     */
-    protected $container;
-
-    /**
-     * Model constructor.
-     *
-     * @param \Rudra\ContainerInterface $container
-     */
-    public function __construct(ContainerInterface $container)
-    {
-        $this->container = $container;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function container(): ContainerInterface
-    {
-        return $this->container;
-    }
+    use ContainerTrait;
+    use DataTrait;
+    use SetContainerTrait;
 }
