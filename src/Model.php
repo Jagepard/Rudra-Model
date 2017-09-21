01e2c86ee28927d207c222a14554e9c1b878ea12
Jagepard <dankorot@gmail.com>
2017-09-21 19:03:47 +0300

    Добавляет debugbar

--- src/Model.php
+++ src/Model.php
@@ -14,5 +14,13 @@ class Model
 
     use ContainerTrait;
     use ControllerTrait;
-    use SetContainerTrait;
+    use SetContainerTrait {
+        SetContainerTrait::__construct as public __setContainerTraitConstruct;
+    }
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->__setContainerTraitConstruct($container);
+        $this->container()->get('debugbar')['time']->startMeasure('Model', 'Model');
+    }
 }
